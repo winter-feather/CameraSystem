daf76d9a24f9b8e6aa7c236d407bfd77466250b8
blackrockfish <blackrockfish@126.com>
2020-04-27 17:57:52 +0800

    v0.1

---
+++ Assets/Script/CameraTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public Vector3 offsetPostion = Vector3.zero;
+    public Transform cmaera;
+    public Transform target;
+    // Use this for initialization
+    private void Start()
+    {
+        transform.position = target.position + offsetPostion;   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //float h = Input.GetAxis("Mouse X") * 0.05f;
+        //float v = Input.GetAxis("Mouse Y") * 0.05f;
+        float h = Input.GetAxis("Horizontal") * 0.05f;
+        float v = Input.GetAxis("Vertical") * 0.05f;
+        offsetPostion.x += h;
+        offsetPostion.y += v;
+        //if (Input.GetMouseButton(2))
+        //{
+        //    p.x += h;
+        //    p.y += v;
+        //}
+        //transform.position = target.position + Quaternion.Euler(-transform.parent.rotation.eulerAngles) * cmaera.rotation * -offsetPostion;// 
+        transform.position = target.position + cmaera.rotation * -offsetPostion;// 
+    }
+
+    public void SetTarget(Transform t) {
+        target = t;
+        offsetPostion = Vector3.zero;
+    }
+}
---
+++ Assets/Script/IM.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class IM : MonoBehaviour
+{
+    float doubleClickTime;
+    public float doubleClickCD;
+    public Vector3 om, md;
+    public TC tc;
+    //public GameObject so;
+    public Action<IM> onDoubleClick, onPressing, onMouseDown, onMouseUp;
+    public int ya;
+    // Use this for initialization
+    void Start()
+    {
+        om = Input.mousePosition;
+        tc.a = Quaternion.identity;
+        tc.b = Quaternion.identity;
+        onDoubleClick += (e) =>
+        {
+            //if (so!=null)
+            //{
+            //    for (int i = 0; i < tc.t.childCount; i++)
+            //    {
+            //        tc.t.transform.GetChild(i).gameObject.SetActive(false);
+            //    }
+            //    so.SetActive(true);
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < tc.t.childCount; i++)
+            //    {
+            //        tc.t.transform.GetChild(i).gameObject.SetActive(true);
+            //    }
+            //}
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoubleCheck();
+        MoseMoveCheck();
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(tc.tc.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition), out hit))
+        //    {
+        //        so = hit.collider.gameObject;
+        //    }
+        //    else
+        //    {
+        //        so = null;
+        //    }
+        //}
+        if (Input.GetMouseButton(0))
+        {
+            tc.RH(-md.x);
+            tc.RV(ya * md.y);
+        }
+        tc.RZ(Input.mouseScrollDelta.y);
+    }
+
+    void DoubleCheck()
+    {
+        doubleClickTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (doubleClickTime > doubleClickCD)
+            {
+                doubleClickTime = 0;
+            }
+            else
+            {
+                if (onDoubleClick != null)
+                {
+                    onDoubleClick.Invoke(this);
+                }
+            }
+        }
+    }
+
+    void MoseMoveCheck()
+    {
+        md = om - Input.mousePosition;
+        om = Input.mousePosition;
+    }
+
+
+}
