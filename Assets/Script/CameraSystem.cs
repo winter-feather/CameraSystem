89a4b8ddfc85929250a2e355a73c8c82aa9f8581
blackrockfish <blackrockfish@126.com>
2020-04-29 16:39:46 +0800

    加入了Bound

--- Assets/Script/CameraSystem.cs
+++ Assets/Script/CameraSystem.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSystem : MonoBehaviour {
+public class CameraSystem : MonoBehaviour
+{
     //-------------------------------------------------------------------
     //public List<Transform> targets;
     //public Vector3 TargetPos
@@ -33,17 +34,21 @@ public class CameraSystem : MonoBehaviour {
     //    }
     //}
     public Transform target, targetCamera;
-    Vector3 targetPos;
-    Quaternion targetRot;
     [HideInInspector]
     public Vector3 offset;
-    public float nzoom, fzoom;
-    [HideInInspector]
-    public float currentzoom, targetzoom;
+    Vector3 targetPos;
+    //-----------------------------------------------
     [HideInInspector]
     public float hAngle, vAngle;
     public Vector3 cameraDefaultDir;
     Quaternion cameraDefaultRot;
+    Quaternion targetRot;
+    //-----------------------------------------------
+    public float nzoom, fzoom;
+    [HideInInspector]
+    public float currentzoom, targetzoom;
+    //-----------------------------------------------
+    public Transform boundMin, boundMax;
     private void Awake()
     {
         Init();
@@ -61,17 +66,32 @@ public class CameraSystem : MonoBehaviour {
         cameraDefaultRot = Quaternion.LookRotation(-cameraDefaultDir);
     }
 
-    void ZoomUpdate() {
-        currentzoom = Mathf.Lerp(currentzoom,targetzoom,Time.deltaTime*10);
+    void ZoomUpdate()
+    {
+        currentzoom = Mathf.Lerp(currentzoom, targetzoom, Time.deltaTime * 10);
     }
 
-    void PosUpdate() {
+    void PosUpdate()
+    {
         targetRot = Quaternion.Euler(hAngle, vAngle, 0);
-        targetPos = target.transform.position + targetRot * cameraDefaultDir * currentzoom + offset;
-        targetCamera.position =  targetPos;
+        targetPos = target.transform.position + offset; ;
+
+    }
+
+    void CameraUpdate()
+    {
+        targetCamera.position = targetPos + targetRot * cameraDefaultDir * currentzoom;
         targetCamera.rotation = targetRot * cameraDefaultRot;
     }
 
+    void BoundUpdate()
+    {
+        if (boundMin == null || boundMax == null) return;
+        targetPos.x = Mathf.Clamp(targetPos.x, boundMin.position.x, boundMax.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, boundMin.position.y, boundMax.position.y);
+        targetPos.z = Mathf.Clamp(targetPos.z, boundMin.position.z, boundMax.position.z);
+        offset = targetPos - target.transform.position;
+    }
     void Update()
     {
         //hAngle += Input.GetAxis("Vertical");
@@ -85,9 +105,12 @@ public class CameraSystem : MonoBehaviour {
 
         ZoomUpdate();
         PosUpdate();
+        BoundUpdate();
+        CameraUpdate();
     }
 
-    public void SetTarget(Transform target) {
+    public void SetTarget(Transform target)
+    {
         this.target = target;
         offset = Vector3.zero;
     }
